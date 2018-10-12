945c9d073b0a6e0dd207a3840ee230d393ff67f0
Steve <steve@debian.edu>
2018-10-12 19:12:10 +0200

    Start of second refactor. Failing test

---
+++ C/newGildedRoseUnitTests.cc
@@ -0,0 +1,40 @@
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTestExt/MockSupport.h>
+
+extern "C" {
+#include "GildedRose.h"
+}
+
+
+TEST_GROUP(TestGildedRoseGroup)
+{
+  void setup() {
+      int result;
+
+      result = add_stock("This is a new mormal item", 19, 23, NORMAL);
+      CHECK_EQUAL(SUCCESS,result);
+
+  }
+  void teardown() {
+     zero_list_size();
+  }
+};
+
+
+
+
+
+
+TEST(TestGildedRoseGroup, FirstTest)
+{
+     FAIL("Fail me\n");
+
+}
+
+
+int main(int ac, char** av)
+{
+
+   return CommandLineTestRunner::RunAllTests(ac, av);
+}
